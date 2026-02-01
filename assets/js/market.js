4add30e8c91644ad7ee2d8dd756f1fbf17260bbd
Evren Kır <ademevrenkir@gmail.com>
2026-02-01 17:42:20 +0300

    Fixed market ticker editor errors by cleaning includes

---
+++ assets/js/market.js
@@ -0,0 +1,15 @@
+new TradingView.widget({
+  "container_id": "tradingview_market_ticker",
+  "symbols": [
+    { "proName": "BIST:XU100", "title": "BIST 100" },
+    { "proName": "FOREXCOM:SPX3500", "title": "S&P 500" },
+    { "proName": "FX_IDC:XAUUSD", "title": "Altın" },
+    { "proName": "FX:USDTRY", "title": "USD/TRY" },
+    { "proName": "BITSTAMP:BTCUSD", "title": "Bitcoin" }
+  ],
+  "showSymbolLogo": true,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "displayMode": "adaptive",
+  "locale": "tr"
+});
